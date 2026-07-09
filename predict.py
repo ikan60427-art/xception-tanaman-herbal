a5075411714be84d0166652dc461fd9d27e08d5f
ikan60427-art <ikan60427@gmail.com>
2026-07-09 22:56:53 +0700

    Update predict.py

--- predict.py
+++ predict.py
@@ -1,55 +1,78 @@
 import os
 import json
-import requests
+import gdown
 import numpy as np
 import tensorflow as tf
 
 from tensorflow.keras.preprocessing import image
 from tensorflow.keras.applications.xception import preprocess_input
 
+# ===========================
+# Path Model & Class
+# ===========================
+
 MODEL_PATH = "model/xception_herbal.keras"
 CLASS_PATH = "model/class_names.json"
 
 # ===========================
-# Download model jika belum ada
+# Download Model jika belum ada
 # ===========================
 
 if not os.path.exists(MODEL_PATH):
 
-    print("Downloading model...")
+    print("Model belum ada. Downloading...")
 
     os.makedirs("model", exist_ok=True)
 
     FILE_ID = "13-czv3xkrMIjD3aYYacPK4NwT2eeL7To"
 
-    URL = f"https://drive.google.com/uc?export=download&id={FILE_ID}"
+    url = f"https://drive.google.com/uc?id={FILE_ID}"
 
-    response = requests.get(URL)
+    gdown.download(
+        url,
+        MODEL_PATH,
+        quiet=False
+    )
 
-    with open(MODEL_PATH, "wb") as f:
-        f.write(response.content)
+    print("Model berhasil didownload.")
 
-    print("Model downloaded.")
-
+# ===========================
+# Load Model
 # ===========================
 
 model = tf.keras.models.load_model(MODEL_PATH)
 
+# ===========================
+# Load Nama Class
+# ===========================
+
 with open(CLASS_PATH, "r") as f:
     class_names = json.load(f)
 
+# ===========================
+# Fungsi Prediksi
+# ===========================
 
 def predict_image(img_path):
 
-    img = image.load_img(img_path, target_size=(299, 299))
+    img = image.load_img(
+        img_path,
+        target_size=(299, 299)
+    )
 
     img_array = image.img_to_array(img)
 
-    img_array = np.expand_dims(img_array, axis=0)
+    img_array = np.expand_dims(
+        img_array,
+        axis=0
+    )
 
     img_array = preprocess_input(img_array)
 
-    prediction = model.predict(img_array, verbose=0)[0]
+    prediction = model.predict(
+        img_array,
+        verbose=0
+    )[0]
 
     index = np.argmax(prediction)
 
