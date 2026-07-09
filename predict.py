fb4bbbe05da5fddc2a9b18a4e9796fa70dabb8ac
ikan60427-art <ikan60427@gmail.com>
2026-07-09 22:05:23 +0700

    Update predict.py

--- predict.py
+++ predict.py
@@ -1,4 +1,6 @@
+import os
 import json
+import requests
 import numpy as np
 import tensorflow as tf
 
@@ -8,37 +10,51 @@ from tensorflow.keras.applications.xception import preprocess_input
 MODEL_PATH = "model/xception_herbal.keras"
 CLASS_PATH = "model/class_names.json"
 
+# ===========================
+# Download model jika belum ada
+# ===========================
+
+if not os.path.exists(MODEL_PATH):
+
+    print("Downloading model...")
+
+    os.makedirs("model", exist_ok=True)
+
+    FILE_ID = "13-czv3xkrMIjD3aYYacPK4NwT2eeL7To"
+
+    URL = f"https://drive.google.com/uc?export=download&id={FILE_ID}"
+
+    response = requests.get(URL)
+
+    with open(MODEL_PATH, "wb") as f:
+        f.write(response.content)
+
+    print("Model downloaded.")
+
+# ===========================
+
 model = tf.keras.models.load_model(MODEL_PATH)
 
-with open(CLASS_PATH,"r") as f:
+with open(CLASS_PATH, "r") as f:
     class_names = json.load(f)
 
 
 def predict_image(img_path):
 
-    img = image.load_img(
-        img_path,
-        target_size=(299,299)
-    )
+    img = image.load_img(img_path, target_size=(299, 299))
 
     img_array = image.img_to_array(img)
 
-    img_array = np.expand_dims(
-        img_array,
-        axis=0
-    )
+    img_array = np.expand_dims(img_array, axis=0)
 
     img_array = preprocess_input(img_array)
 
-    prediction = model.predict(
-        img_array,
-        verbose=0
-    )[0]
+    prediction = model.predict(img_array, verbose=0)[0]
 
     index = np.argmax(prediction)
 
     label = class_names[index]
 
-    confidence = float(prediction[index])*100
+    confidence = float(prediction[index]) * 100
 
     return label, confidence
